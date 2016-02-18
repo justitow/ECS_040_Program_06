6d61964ba895899e07d5fa3d7dbb0d8f583e17b3
justitow <justitow@gmail.com>
2016-02-18 14:24:03 -0800

    first commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ECS_040_Program_06
+//
+//  Created by Justin Weich on 2/18/16.
+//  Copyright © 2016 Justin Weich. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
